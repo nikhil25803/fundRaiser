046ae0eb973480f601c09a16bc0fc14fed8ccc20
Nikhil Raj <nikhil25803@gmail.com>
2022-07-31 21:09:24 +0530

    models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+import datetime
 # Create your models here.
+
+
+class PostModel(models.Model):
+
+    title = models.CharField(max_length=20)
+    heading = models.CharField(max_length=50)
+    description = models.CharField(max_length=100)
+    posted_id = models.IntegerField()
+    posted_by = models.CharField(max_length=20)
+    posted_on = models.DateField('Date', default=datetime.date.today)
+    upi_id = models.CharField(max_length=50)
+
+
+    pass
--- raiser/settings.py
+++ raiser/settings.py
@@ -9,10 +9,11 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
+import imp
+from pathlib import Path
 import os
 from dotenv import load_dotenv
 load_dotenv()
-from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -41,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'app',
     "django_browser_reload",
-    'crispy_forms',
+    
 ]
 
 MIDDLEWARE = [
@@ -126,15 +127,19 @@ STATICFILES_DIRS = [
     BASE_DIR/'static'
 ]
 
+
+MEDIA_URL = '/mediafiles/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend' 
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.mailazy.com'
 EMAIL_HOST_USER = str(os.getenv('ACCESS'))
 EMAIL_HOST_PASSWORD = str(os.getenv('KEY'))
-EMAIL_PORT = 587 
-EMAIL_USE_TLS = True 
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True

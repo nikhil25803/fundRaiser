e81425fbad54a81520e4d4c90b8d7263df320fec
Nikhil Raj <nikhil25803@gmail.com>
2022-07-31 13:14:09 +0530

    dotenv setting

--- raiser/settings.py
+++ raiser/settings.py
@@ -132,12 +132,6 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_HOST_USER = 'nikhil25803@gmail.com'
-# EMAIL_HOST_PASSWORD = 'Msd@7880409'
-# EMAIL_USE_TLS = True
-
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend' 
 EMAIL_HOST = 'smtp.mailazy.com'
 EMAIL_HOST_USER = str(os.getenv('ACCESS'))

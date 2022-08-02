585b4ba84f0284796b8e02bcc3914876d538df16
Nikhil Raj <nikhil25803@gmail.com>
2022-08-02 21:53:55 +0530

    Added Models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,17 @@
-from django.db import models
 
+from django.db import models
+import datetime
 # Create your models here.
+
+class NewPostModel(models.Model):
+
+    title = models.CharField(max_length=20)
+    description = models.CharField(max_length=100)
+    images = models.ImageField(upload_to='images')
+    posted_on = models.DateField(("Date"), default=datetime.date.today)
+    posted_by = models.CharField(max_length=20)
+    post_id = models.CharField(max_length=10)
+
+    def __str(self):
+
+        return f"{self.title} by {self.posted_on}"

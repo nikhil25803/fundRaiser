36419d034c4d46f3f03d09e287a6e6f7811fe30b
Nikhil Raj <nikhil25803@gmail.com>
2022-08-04 03:26:37 +0530

    Hosting

--- raiser/settings.py
+++ raiser/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://funds-raiser.herokuapp.com/', '127.0.0.1']
 
 
 # Application definition

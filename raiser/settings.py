fbf98ac83a91e1b6abc628916aea03c4d1988c4e
Nikhil Raj <nikhil25803@gmail.com>
2022-08-04 03:34:42 +0530

    Collect Static Changes

--- raiser/settings.py
+++ raiser/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -128,7 +129,7 @@ STATICFILES_DIRS = [
     BASE_DIR/'static'
 ]
 
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')

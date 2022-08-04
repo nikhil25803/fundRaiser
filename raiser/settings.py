834fdbf991956de6855adced5176d0258a1efe1c
Nikhil Raj <nikhil25803@gmail.com>
2022-08-04 11:03:03 +0530

    Edits

--- raiser/settings.py
+++ raiser/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://funds-raiser.herokuapp.com/', 'localhost', '127.0.0.1']
 
 
 # Application definition

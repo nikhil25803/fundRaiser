2249b52be04b5f82a297f0c0438e8d0a260df417
Nikhil Raj <nikhil25803@gmail.com>
2022-08-04 18:56:45 +0530

    collect static

--- raiser/settings.py
+++ raiser/settings.py
@@ -125,6 +125,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR/'static'
 ]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
